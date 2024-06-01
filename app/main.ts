bd9d4894d53c99b0f8bd2197688e8d4b9ec0fd16
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2024-06-02 05:17:09 +0700

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -1,8 +1,15 @@
 import * as net from "net";
 
 const server = net.createServer((socket) => {
-  socket.write("HTTP/1.1 200 OK\r\n\r\n");
-  socket.end();
+  socket.on("data", (data) => {
+    const path = data.toString().split(" ")[1];
+    if (path === "/") {
+      socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    } else {
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+    }
+    socket.end();
+  });
 });
 
 server.listen(4221, "localhost", () => {
